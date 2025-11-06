b42d9ec88ceed663e7c968f80d2adf7d4cf01aed
Gabriel Tremblay <gtremblay@Athena.local>
2025-11-06 08:21:53 -0500

    new lsp config for nvim 0.11

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,29 +1,6 @@
-local on_attach = require("nvchad.configs.lspconfig").on_attach local on_init = require("nvchad.configs.lspconfig").on_init
-local capabilities = require("nvchad.configs.lspconfig").capabilities
+require("nvchad.configs.lspconfig").defaults()
 
-local lspconfig = require "lspconfig"
-local util = require "lspconfig.util"
+local servers = { "html", "cssls" }
+vim.lsp.enable(servers)
 
--- local servers = { "html", "cssls" }
--- lsps with default config
--- for _, lsp in ipairs(servers) do
---   lspconfig[lsp].setup {
---     on_attach = on_attach,
---     on_init = on_init,
---     capabilities = capabilities,
---   }
--- end
-
-lspconfig.clangd.setup {
-  on_attach = function (client, bufnr)
-    client.server_capabilities.signatureHelpProvider = false
-    on_attach(client, bufnr)
-  end,
-  capabilities = capabilities,
-}
-
-lspconfig.pyright.setup({
-  on_attach = on_attach,
-  capabilities = capabilities,
-  filetypes = {"python"},
-})
+-- read :h vim.lsp.config for changing options of lsp servers 
